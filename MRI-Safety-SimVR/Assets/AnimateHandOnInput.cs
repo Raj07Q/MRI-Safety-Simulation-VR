ae8594173ff433dfc59931aecd1614d88dcd31a4
Raj07Q <rahul.rajd@quest-global.com>
2023-04-03 13:30:57 +0530

    New environment updated

---
+++ MRI-Safety-SimVR/Assets/AnimateHandOnInput.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAction;
+    public InputActionProperty gripAction;
+    public InputActionProperty A_Action;
+
+    public Animator handAnimator;
+    //public GameObject virtualKeyboard;
+
+    //private bool IsPressed;
+
+    
+    void Awake()
+    {
+        A_Action.action.performed += Button_APressed;
+
+        //virtualKeyboard.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        A_Action.action.performed -= Button_APressed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pinchValue = pinchAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", pinchValue);
+
+        float gripValue = gripAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+
+        //A_Action.action.performed += context =>
+        //{
+            
+        //};
+        
+    }
+
+    public void Button_APressed(InputAction.CallbackContext context)
+    {
+        Debug.Log("A Pressed!!!!!!!!!!!!!!");
+        SceneManager.LoadScene("Test-MainVR");
+        //if (!IsPressed)
+        //{
+        //    virtualKeyboard.SetActive(true);
+
+        //    IsPressed = true;
+        //}
+        //else
+        //{
+        //    virtualKeyboard.SetActive(false);
+
+        //    IsPressed = false;
+        //}
+    }
+}
---
+++ MRI-Safety-SimVR/Assets/WheelchairController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class WheelchairController : MonoBehaviour
+{
+    public XROrigin rig;
+    public ActionBasedContinuousMoveProvider cont;
+
+    public InputActionProperty gripAction;
+    public GameObject player;
+    //public GameObject wheelChair;
+
+    private bool IsEntered;
+    private float gripValue;
+
+    private void Awake()
+    {
+        rig = FindObjectOfType<XROrigin>();
+    }
+
+    private void FixedUpdate()
+    {
+        gripValue = gripAction.action.ReadValue<float>();
+
+        if(gripValue > 0.9f && IsEntered)
+        {
+            //Debug.Log("Grip Pressed!!!!!");
+            this.transform.parent = player.transform;
+            cont.moveSpeed = 0.45f;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Hands"))
+        {
+            IsEntered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hands"))
+        {
+            IsEntered = false;
+            this.transform.parent = null;
+            cont.moveSpeed = 0.75f;
+        }
+    }
+}
